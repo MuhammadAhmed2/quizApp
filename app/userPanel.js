d2bcef184d87257b39d6e4854f813b2bdaf67b26
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-06-10 16:55:53 +0500

    userPanel Update

--- app/userPanel.js
+++ app/userPanel.js
@@ -1,5 +1,6 @@
 let userPronoun = document.querySelector('.userName');
 let coursesDiv = document.querySelector('.courses');
+let score = document.querySelector('.score');
 
 // importing User data
 let user = JSON.parse(localStorage.getItem('user'));
@@ -11,31 +12,41 @@ const coursesArr = [
         courseThumbnail: 'https://i.ytimg.com/vi/ER9SspLe4Hg/maxresdefault.jpg',
         youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR',
         courseDescription: "JavaScript Course in Hindi: This Javascript tutorial in Hindi course is designed for beginners with an aim to take JavaScript/ES6 and its concepts to an advanced level. Master JavaScript with this course now and you will be able to create a website easily after watching this.",
-        language: 'Urdu',
+        language: 'Urdu/Hindi',
         instructor: 'Code With Harry',
         price: 0
     },
     {
-        courseName: "Python for Everybody",
-        courseThumbnail: 'https://i.ytimg.com/vi/8DvywoWv6fI/maxresdefault.jpg',
-        youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PLu0W_9lII9agRSgFoJdaakJtHnO4OgVwP',
+        courseName: "Beginner's Python Tutorials ",
+        courseThumbnail: 'https://i.ytimg.com/vi/fqF9M92jzUo/maxresdefault.jpg',
+        youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PLu0W_9lII9agICnT8t4iYVSZ3eykIAOME',
         courseDescription: "This Python for Everybody course is designed for beginners and intermediate programmers. It covers the basics of Python, including syntax, data structures, and libraries, and moves on to more advanced topics.",
-        language: 'English',
-        instructor: 'Dr. Chuck',
+        language: 'Urdu/Hindi',
+        instructor: 'Harry',
         price: 0
     },
     {
         courseName: "ReactJS Tutorial",
         courseThumbnail: 'https://i.ytimg.com/vi/sBws8MSXN7A/maxresdefault.jpg',
-        youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PLu0W_9lII9agICnT8t4iYVSZ3eykIAOME',
+        youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS',
         courseDescription: "This ReactJS tutorial is aimed at helping beginners get started with React. The course covers components, state, props, lifecycle methods, and more. By the end of this tutorial, you will be able to build a dynamic web application using ReactJS.",
         language: 'English',
         instructor: 'Academind',
         price: 0
-    }
+    },
+    {
+        courseName: "Photoshop for Beginners",
+        courseThumbnail: 'https://i.ytimg.com/vi/rL9iKGkgOHI/maxresdefault.jpg',
+        youtubePlaylistLink: 'https://www.youtube.com/playlist?list=PLW-zSkCnZ-gA5Jn6gZtUa6-aG0OoRZyb6',
+        courseDescription: "Hello everyone. This is my new series of Photoshop for beginners in Urdu / Hindi language. Although this is for beginners but even seasoned Photoshop user can also benefit with these videos as this is the most detailed course on Photoshop available in Urdu / Hindi Language.",
+        language: 'Urdu/Hindi',
+        instructor: 'Imran Ali Dina',
+        price: 0
+    },
 ];
 
 userPronoun.innerHTML = user[0].userName;
+score.innerHTML = `Your Over All Score is <span style="font-weight: 800;">${user[0].overallScore}/150</span>`;
 
 let courseList = coursesArr.map((course)=>{
     coursesDiv.innerHTML += `
@@ -47,7 +58,7 @@ let courseList = coursesArr.map((course)=>{
                     <h4>Course Description:</h4>
                     <p>${course.courseDescription}</p>
                     <h4 class="price">Price: ${course.price === 0 ? 'Free' : '$' + course.price}</h4>
-                    <a class="watch-btn" href="${course.youtubePlaylistLink}" target="_blank">Watch on YouTube</a>
+                    <a class="watch-btn" href="${course.youtubePlaylistLink}" target="_blank">Start Learning</a>
                 </div>
             </div>
 
